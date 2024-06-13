c0dc79cbd0ca1334578cd67e5644b833da0ccfe9
Aurora <19178435+aurora-a-k-a-lightning@users.noreply.github.com>
2024-06-13 09:40:12 -0700

    spruce/address-api-migration: migrate the address api verification to the mock js api (#54)
    * spruce/address-api-migration
    - move mock address verification api to mock js file/server
    
    * spruce/address-api-migration
    - move mock address verification api to mock js file/server

--- src/applications/spruce-challenge-app/mocks/index.js
+++ src/applications/spruce-challenge-app/mocks/index.js
@@ -4,6 +4,55 @@ const delay = require('mocker-api/lib/delay');
 const commonResponses = require('../../../platform/testing/local-dev-mock-api/common');
 // const checkInData = require('./mocks/v2/check-in-data/index');
 
+function requiredParams() {
+  return ['AddressLine1', 'City', 'State', 'ZipCode'];
+}
+
+function verifyRequiredParams(params) {
+  let verified = true;
+  const missingParams = [];
+  requiredParams().forEach(param => {
+    if (!Object.keys(params).includes(param)) {
+      verified = false;
+      missingParams.push(param);
+    }
+  });
+  return { verified, missingParams };
+}
+
+function handleErrorCases(params) {
+  const errorCases = [
+    '401-unauthorized',
+    '403-forbidden',
+    '404-not-found',
+    '500-internal-server-error',
+    '503-service-unavailable',
+  ];
+
+  let text = '';
+
+  Object.values(params).forEach(v => {
+    if (errorCases.includes(v.toLowerCase()) && text === '') {
+      text = v;
+    }
+  });
+
+  switch (text) {
+    case '401-unauthorized':
+      return { httpStatus: 401, errorMessage: 'Unauthorized' };
+    case '403-forbidden':
+      return { httpStatus: 403, errorMessage: 'Forbidden' };
+    case '404-not-found':
+      return { httpStatus: 404, errorMessage: 'Resource not found' };
+    case '500-internal-server-error':
+      return { httpStatus: 500, errorMessage: 'Internal Server Error' };
+    case '503-service-unavailable':
+      return { httpStatus: 503, errorMessage: 'Service Unavailable' };
+    default:
+      return { httpStatus: 0, errorMessage: null };
+  }
+}
+
 const responses = {
   ...commonResponses,
   'PUT /v0/in_progress_forms/24-SPRUCE': (req, res) => {
@@ -11,6 +60,41 @@ const responses = {
   },
   'POST /check_in/v0/travel_claims/': (req, res) => {
     return res.json({ data: req.body });
+  },
+  'GET /vetsapi/verifyAddress': (req, res) => {
+    const params = req.query;
+
+    const { httpStatus, errorMessage } = handleErrorCases(params);
+
+    if (httpStatus > 0) {
+      return res.json({
+        'Error Message': errorMessage,
+        'Error Code': httpStatus.toString(),
+      });
+    }
+
+    const { verified, missingParams } = verifyRequiredParams(params);
+    if (!verified) {
+      return res.json({
+        'Error Message': `Missing required fields to verify address: ${missingParams.join(
+          ', ',
+        )}.`,
+        'Error Code': '400',
+      });
+    }
+
+    const suggestedResults = {};
+    Object.entries(params).forEach(e => {
+      suggestedResults[e[0]] = e[1].toUpperCase();
+    });
+
+    return res.json({
+      AddressLine1: suggestedResults.AddressLine1,
+      AddressLine2: suggestedResults.AddressLine2,
+      City: suggestedResults.City,
+      State: suggestedResults.State,
+      'Zip Code': suggestedResults.ZipCode,
+    });
   },
 };
 
